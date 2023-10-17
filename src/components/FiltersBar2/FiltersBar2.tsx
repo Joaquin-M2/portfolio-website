8daf18bd845330d229b6c8537065641a60f96c24
Joaquin-M2 <joaquin.mmol@gmail.com>
2023-10-17 11:30:32 +0200

    Add "FILTERS" button to FiltersBar2
    So the filter panel (Tools page) can be hidden when the user doesn't need it.

--- src/components/FiltersBar2/FiltersBar2.tsx
+++ src/components/FiltersBar2/FiltersBar2.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useEffect, useRef, useState } from "react";
 import Tag from "../ToolCard/Tag/Tag";
 
 import styles from "./filtersBar2.module.scss";
@@ -11,6 +12,10 @@ function FiltersBar2({
   handleRemoveFilterTag,
   tags,
 }) {
+  const toggleButton = useRef<HTMLInputElement>();
+  const blackSail = useRef<HTMLDivElement>();
+  const [buttonIsChecked, setButtonIsChecked] = useState(true);
+
   const dropdownOptions = tags.length ? (
     tags
       .filter((tag) => !selectedFilterTags.includes(tag.name))
@@ -22,39 +27,69 @@ function FiltersBar2({
   ) : (
     <option>Loading...</option>
   );
+
+  useEffect(() => {
+    if (toggleButton.current.checked === false) {
+      blackSail.current.style.display = "block";
+    } else {
+      blackSail.current.style.display = "none";
+    }
+  }, [buttonIsChecked]);
+
   return (
     <>
-      <div className={styles.container}>
-        <div className={styles.innerContainer}>
-          <fieldset className={styles.titleFieldset}>
-            <legend className={styles.titleLegend}>Filter by Title</legend>
-            <input
-              className={styles.searchInput}
-              type="search"
-              placeholder="Search by tool title - Case sensitive"
-              maxLength={25}
-              onChange={filterBySearchFunction}
-            />
-          </fieldset>
-          <fieldset className={styles.tagFieldset}>
-            <legend className={styles.tagLegend}>Filter by Tag</legend>
-            <select className={styles.selectInput} name="tags" multiple>
-              {dropdownOptions}
-            </select>
-            <div className={styles.tagsContainer}>
-              {selectedFilterTags.map((tag, id) => (
-                <Tag
-                  key={id}
-                  isFilterButton
-                  handleRemoveFilterTag={handleRemoveFilterTag}
-                >
-                  {tag}
-                </Tag>
-              ))}
-            </div>
-          </fieldset>
-        </div>
-      </div>
+      <input
+        ref={toggleButton}
+        className={styles.FiltersPanelCheckbox}
+        type="checkbox"
+        id="filters-panel"
+        name="slider-filters-panel"
+        checked={buttonIsChecked}
+        onChange={() => {
+          setButtonIsChecked((prevState) => !prevState);
+        }}
+      />
+      <aside className={styles.container}>
+        <label
+          className={styles.FiltersPanelButton}
+          htmlFor="filters-panel"
+        ></label>
+        <fieldset className={styles.titleFieldset}>
+          <legend className={styles.titleLegend}>Filter by Title</legend>
+          <input
+            className={styles.searchInput}
+            type="search"
+            placeholder="Search by tool title - Case sensitive"
+            maxLength={25}
+            onChange={filterBySearchFunction}
+          />
+        </fieldset>
+        <fieldset className={styles.tagFieldset}>
+          <legend className={styles.tagLegend}>Filter by Tag</legend>
+          <select className={styles.selectInput} name="tags" multiple>
+            {dropdownOptions}
+          </select>
+          <div className={styles.tagsContainer}>
+            {selectedFilterTags.map((tag, id) => (
+              <Tag
+                key={id}
+                isFilterButton
+                handleRemoveFilterTag={handleRemoveFilterTag}
+              >
+                {tag}
+              </Tag>
+            ))}
+          </div>
+        </fieldset>
+      </aside>
+      <div
+        ref={blackSail}
+        className={styles.blackSail}
+        onClick={() => {
+          toggleButton.current.checked = true;
+          setButtonIsChecked((prevState) => !prevState);
+        }}
+      ></div>
     </>
   );
 }
