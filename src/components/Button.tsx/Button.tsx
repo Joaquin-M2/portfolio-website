4003afada59fa5e22294bfab2ddd1d1c3ff8a49c
Joaquin-M2 <joaquin.mmol@gmail.com>
2023-10-23 20:44:27 +0200

    Adapt "Button" component to be used in forms

--- src/components/Button.tsx/Button.tsx
+++ src/components/Button.tsx/Button.tsx
@@ -6,15 +6,19 @@ import styles from "./button.module.scss";
 
 interface ButtonProps {
   children: JSX.Element | JSX.Element[] | string;
+  form?: string;
   onClick?: React.MouseEventHandler<HTMLButtonElement>;
   small?: boolean;
+  type?: "button" | "submit" | "reset";
 }
 
-function Button({ children, onClick, small }: ButtonProps) {
+function Button({ children, form, onClick, small, type }: ButtonProps) {
   return (
     <button
       className={`${styles.button} ${small && styles.buttonSmall}`}
+      form={form}
       onClick={onClick}
+      type={type || "button"}
     >
       {children}
     </button>
