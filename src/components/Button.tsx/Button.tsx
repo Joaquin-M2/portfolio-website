340ea527729066b3e2dd7063260e8f842c4801cd
Joaquin-M2 <joaquin.mmol@gmail.com>
2023-10-27 23:36:38 +0200

    Add "disabled" prop to Button component

--- src/components/Button.tsx/Button.tsx
+++ src/components/Button.tsx/Button.tsx
@@ -6,16 +6,26 @@ import styles from "./button.module.scss";
 
 interface ButtonProps {
   children: JSX.Element | JSX.Element[] | string;
+  disabled?: boolean;
   form?: string;
   onClick?: React.MouseEventHandler<HTMLButtonElement>;
   small?: boolean;
   type?: "button" | "submit" | "reset";
 }
 
-function Button({ children, form, onClick, small, type }: ButtonProps) {
+function Button({
+  children,
+  disabled,
+  form,
+  onClick,
+  small,
+  type,
+}: ButtonProps) {
   return (
     <button
-      className={`${styles.button} ${small && styles.buttonSmall}`}
+      className={`${styles.button} ${small && styles.buttonSmall} ${
+        disabled && styles.buttonDisabled
+      }`}
       form={form}
       onClick={onClick}
       type={type || "button"}
