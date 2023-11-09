065b3287581e9fc3e3c9196dae225567eaf45755
Joaquin-M2 <joaquin.mmol@gmail.com>
2023-11-09 12:24:08 +0100

    Split "FormInModal" into ad hoc form components
    The differences between forms made them hardly reusable, making "FormInModal" a hard to maintain component.

---
+++ src/app/tools/AddToolForm.tsx
@@ -0,0 +1,223 @@
+import React, {
+  Dispatch,
+  MouseEventHandler,
+  SetStateAction,
+  useEffect,
+  useState,
+} from "react";
+import Modal from "../../components/Modal/Modal";
+import Form from "../../components/Form/Form";
+import Input from "../../components/Form/Input/Input";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { createRequest } from "../../utils/requests";
+import { urlRegExp } from "../../utils/regular-expressions";
+
+interface AddToolFormProps {
+  formIsOpen: boolean;
+  handleAddTag?: (event) => void;
+  handleRemoveTag?: (event) => void;
+  hideModal: MouseEventHandler;
+  id?: string;
+  requestMethod: string;
+  requestUrlPath: string;
+  resetFormValues?: boolean;
+  selectedTagsAddToolForm?: any[];
+  setToolsFrontend?: Dispatch<SetStateAction<any[]>>;
+  tags?: any[];
+}
+
+interface FormInputs {
+  description: string;
+  iconUrl: string;
+  tags: string[];
+  title: string;
+  url: string;
+}
+
+function AddToolForm({
+  formIsOpen,
+  handleAddTag,
+  handleRemoveTag,
+  hideModal,
+  id,
+  requestMethod,
+  requestUrlPath,
+  resetFormValues,
+  selectedTagsAddToolForm,
+  setToolsFrontend,
+
+  tags,
+}: AddToolFormProps) {
+  const [formResponse, setFormResponse] = useState({
+    status: 500,
+    message: "",
+  });
+  const [requestIsSuccessful, setRequestIsSuccessful] = useState(false);
+
+  useEffect(() => {
+    if (!formIsOpen) {
+      reset();
+      setTimeout(() => {
+        setFormResponse((prevValue) => ({ ...prevValue, message: "" }));
+        setRequestIsSuccessful(false);
+      }, 500); // Time until CSS transition finishes.
+    }
+    if (
+      !formIsOpen &&
+      formResponse.message &&
+      requestUrlPath.startsWith("/tools")
+    ) {
+      setTimeout(() => {
+        setToolsFrontend((prevValue) => [...prevValue, id]);
+      }, 500); // Time until CSS transition finishes.
+    }
+  }, [formIsOpen]);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+    watch,
+  } = useForm<FormInputs>();
+
+  const onSubmit: SubmitHandler<FormInputs> = async (data) => {
+    data.tags = selectedTagsAddToolForm.map((selectedTag) => {
+      return selectedTag.id;
+    }); // Otherwise it would just pick the tags marked in the "Select tag(s)" dropdown.
+    try {
+      console.log(data);
+      const response = await fetch(
+        createRequest({
+          urlPath: requestUrlPath,
+          method: requestMethod,
+          requestBody: data,
+        })
+      );
+
+      const result = await response.json();
+
+      setFormResponse({
+        status: response.status,
+        message: result.message,
+      });
+
+      if (response.status >= 200 && response.status < 400) {
+        setRequestIsSuccessful(true);
+        if (requestUrlPath === "/user/login") {
+          localStorage.setItem("userToken", result.token);
+        }
+      }
+    } catch (error) {
+      setFormResponse({
+        status: 500,
+        message: error.message,
+      });
+      throw error.message;
+    }
+  };
+
+  return (
+    <Modal
+      backendResponse={formResponse}
+      isShown={formIsOpen}
+      hideModal={hideModal}
+      targetForm={`add-tool-form-${id}`}
+      title="Add Tool"
+      requestIsSuccessful={requestIsSuccessful}
+    >
+      <Form
+        hasFieldset
+        id={`add-tool-form-${id}`}
+        legend="Add tool form"
+        onSubmit={handleSubmit(onSubmit)}
+        resetFormValues={resetFormValues}
+      >
+        <Input
+          aria-invalid={errors.title ? true : false}
+          watchedValue={watch("title")}
+          error={
+            errors.title && "Title needs to be between 3 and 50 characters."
+          }
+          formIsOpen={formIsOpen}
+          id="add-tool-title-input"
+          placeholder="Title"
+          required
+          type="text"
+          {...register("title", {
+            required: true,
+            minLength: 3,
+            maxLength: 50,
+          })}
+        />
+        <Input
+          aria-invalid={errors.description ? true : false}
+          watchedValue={watch("description")}
+          error={
+            errors.description &&
+            "Description needs to be between 10 and 250 characters."
+          }
+          formIsOpen={formIsOpen}
+          id="add-tool-description-input"
+          placeholder="Description"
+          required
+          type="textarea"
+          {...register("description", {
+            required: true,
+            minLength: 10,
+            maxLength: 250,
+          })}
+        />
+        <Input
+          handleAddTag={handleAddTag}
+          handleRemoveTag={handleRemoveTag}
+          selectedTagsAddToolForm={selectedTagsAddToolForm}
+          tags={tags}
+          aria-invalid={errors.tags ? true : false}
+          watchedValue={watch("tags")}
+          error={errors.tags && "You need to select at least 1 tag."}
+          formIsOpen={formIsOpen}
+          id="add-tool-select-tag(s)-input"
+          placeholder="Select tag(s)"
+          //required
+          type="selectMultiple"
+          {...register("tags", {
+            //required: true,
+          })}
+          //onBlur={() => setValue("tags", selectedTagsAddToolForm)}
+        />
+        <Input
+          aria-invalid={errors.iconUrl ? true : false}
+          watchedValue={watch("iconUrl")}
+          error={
+            errors.iconUrl && "Invalid URL. Accepted format: <domain>/<path>"
+          }
+          formIsOpen={formIsOpen}
+          id="add-tool-icon-url-input"
+          placeholder="Icon URL"
+          required
+          type="url"
+          {...register("iconUrl", {
+            pattern: urlRegExp,
+          })}
+        />
+        <Input
+          aria-invalid={errors.url ? true : false}
+          watchedValue={watch("url")}
+          error={errors.url && "Invalid URL. Accepted format: <domain>/<path>"}
+          formIsOpen={formIsOpen}
+          id="add-tool-url-input"
+          placeholder="Tool URL"
+          required
+          type="url"
+          {...register("url", {
+            required: true,
+            pattern: urlRegExp,
+          })}
+        />
+      </Form>
+    </Modal>
+  );
+}
+
+export default AddToolForm;
---
+++ src/app/tools/LoginAndSignupForms.tsx
@@ -0,0 +1,176 @@
+import React, {
+  Dispatch,
+  MouseEventHandler,
+  SetStateAction,
+  useEffect,
+  useState,
+} from "react";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { createRequest } from "../../utils/requests";
+import Input from "../../components/Form/Input/Input";
+import { emailRegExp } from "../../utils/regular-expressions";
+import Form from "../../components/Form/Form";
+import Modal from "../../components/Modal/Modal";
+
+interface LoginAndSignupFormsProps {
+  formIsOpen: boolean;
+  formType: "login" | "signup";
+  hideModal: MouseEventHandler;
+  id?: string;
+  requestMethod: string;
+  requestUrlPath: string;
+  resetFormValues?: boolean;
+  setToolsFrontend?: Dispatch<SetStateAction<any[]>>;
+}
+
+interface FormInputs {
+  email: string;
+  password: string;
+}
+
+function LoginAndSignupForms({
+  formIsOpen,
+  formType,
+  hideModal,
+  id,
+  requestMethod,
+  requestUrlPath,
+  resetFormValues,
+  setToolsFrontend,
+}: LoginAndSignupFormsProps) {
+  const [formResponse, setFormResponse] = useState({
+    status: 500,
+    message: "",
+  });
+  const [requestIsSuccessful, setRequestIsSuccessful] = useState(false);
+
+  useEffect(() => {
+    if (!formIsOpen) {
+      reset();
+      setTimeout(() => {
+        setFormResponse((prevValue) => ({ ...prevValue, message: "" }));
+        setRequestIsSuccessful(false);
+      }, 500); // Time until CSS transition finishes.
+    }
+    if (
+      !formIsOpen &&
+      formResponse.message &&
+      requestUrlPath.startsWith("/tools")
+    ) {
+      setTimeout(() => {
+        setToolsFrontend((prevValue) => [...prevValue, id]);
+      }, 500); // Time until CSS transition finishes.
+    }
+    //console.log(errors);
+  }, [formIsOpen]);
+
+  let modalTitle;
+  let formLegend;
+  switch (formType) {
+    case "login":
+      modalTitle = "Log In";
+      formLegend = "Log in form";
+      break;
+    case "signup":
+      modalTitle = "Sign Up";
+      formLegend = "Sign up form";
+      break;
+    default:
+      modalTitle = "Form";
+      formLegend = "Form";
+      break;
+  }
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+    watch,
+  } = useForm<FormInputs>();
+
+  const onSubmit: SubmitHandler<FormInputs> = async (data) => {
+    try {
+      const response = await fetch(
+        createRequest({
+          urlPath: requestUrlPath,
+          method: requestMethod,
+          requestBody: data,
+        })
+      );
+
+      const result = await response.json();
+
+      setFormResponse({
+        status: response.status,
+        message: result.message,
+      });
+
+      if (response.status >= 200 && response.status < 400) {
+        setRequestIsSuccessful(true);
+        if (requestUrlPath === "/user/login") {
+          localStorage.setItem("userToken", result.token);
+        }
+      }
+    } catch (error) {
+      setFormResponse({
+        status: 500,
+        message: error.message,
+      });
+      throw error.message;
+    }
+  };
+  return (
+    <Modal
+      backendResponse={formResponse}
+      isShown={formIsOpen}
+      hideModal={hideModal}
+      targetForm={`${formType}-form-${id}`}
+      title={modalTitle}
+      requestIsSuccessful={requestIsSuccessful}
+    >
+      <Form
+        hasFieldset
+        id={`${formType}-form-${id}`}
+        legend={formLegend}
+        onSubmit={handleSubmit(onSubmit)}
+        resetFormValues={resetFormValues}
+      >
+        <Input
+          aria-invalid={errors.email ? true : false}
+          watchedValue={watch("email")}
+          error={
+            errors.email &&
+            "Invalid email. Accepted format: <username>@<domain>"
+          }
+          formIsOpen={formIsOpen}
+          id={`${formType}-email-input`}
+          placeholder="Email"
+          required
+          type="email"
+          {...register("email", { required: true, pattern: emailRegExp })}
+        />
+        <Input
+          aria-invalid={errors.password ? true : false}
+          watchedValue={watch("password")}
+          error={
+            errors.password &&
+            "Password needs to be between 6 and 20 characters."
+          }
+          formIsOpen={formIsOpen}
+          id={`${formType}-password-input`}
+          placeholder="Password"
+          required
+          type="password"
+          {...register("password", {
+            required: true,
+            minLength: 6,
+            maxLength: 20,
+          })}
+        />
+      </Form>
+    </Modal>
+  );
+}
+
+export default LoginAndSignupForms;
