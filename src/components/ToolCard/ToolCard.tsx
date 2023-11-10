2dabd864b6846d6df10833db78fc57d2f8f703e0
Joaquin-M2 <joaquin.mmol@gmail.com>
2023-11-10 20:34:14 +0100

    Use "UpdateToolForm" in "ToolCard" component

--- src/components/ToolCard/ToolCard.tsx
+++ src/components/ToolCard/ToolCard.tsx
@@ -14,17 +14,25 @@ import MenuCard from "../MenuCard/MenuCard";
 
 import styles from "./toolCard.module.scss";
 import DeleteToolForm from "./DeleteToolForm/DeleteToolForm";
+import UpdateToolForm from "./UpdateToolForm/UpdateToolForm";
 
 interface ToolCardProps {
   backendResponseDeleteTool?: { status: number; message: string };
   setDeleteToolModalIsShown: MouseEventHandler;
+  setUpdateToolModalIsShown: MouseEventHandler;
   deleteToolModalIsShown: boolean;
+  updateToolModalIsShown: boolean;
   description: string;
+  handleAddTag: (event) => void;
+  handleRemoveTag: (event) => void;
   hideDeleteToolModal: MouseEventHandler;
+  hideUpdateToolModal: MouseEventHandler;
   id: string;
   logo: string | StaticImageData;
   setToolsFrontend?: Dispatch<SetStateAction<any[]>>;
-  tags: { _id: string; name: string }[];
+  selectedTagsAddToolForm: any[];
+  allTags: any[];
+  toolTags: { _id: string; name: string }[];
   title: string;
   toolsInLocalStorage: string[];
   url: string;
@@ -32,13 +40,20 @@ interface ToolCardProps {
 
 export default function ToolCard({
   setDeleteToolModalIsShown,
+  setUpdateToolModalIsShown,
   setToolsFrontend,
+  selectedTagsAddToolForm,
   deleteToolModalIsShown,
+  updateToolModalIsShown,
   description,
+  handleAddTag,
+  handleRemoveTag,
   hideDeleteToolModal,
+  hideUpdateToolModal,
   id,
   logo,
-  tags,
+  allTags,
+  toolTags,
   title,
   toolsInLocalStorage,
   url,
@@ -123,7 +138,7 @@ export default function ToolCard({
                       <path d="M495.9 166.6c3.2 8.7 .5 18.4-6.4 24.6l-43.3 39.4c1.1 8.3 1.7 16.8 1.7 25.4s-.6 17.1-1.7 25.4l43.3 39.4c6.9 6.2 9.6 15.9 6.4 24.6c-4.4 11.9-9.7 23.3-15.8 34.3l-4.7 8.1c-6.6 11-14 21.4-22.1 31.2c-5.9 7.2-15.7 9.6-24.5 6.8l-55.7-17.7c-13.4 10.3-28.2 18.9-44 25.4l-12.5 57.1c-2 9.1-9 16.3-18.2 17.8c-13.8 2.3-28 3.5-42.5 3.5s-28.7-1.2-42.5-3.5c-9.2-1.5-16.2-8.7-18.2-17.8l-12.5-57.1c-15.8-6.5-30.6-15.1-44-25.4L83.1 425.9c-8.8 2.8-18.6 .3-24.5-6.8c-8.1-9.8-15.5-20.2-22.1-31.2l-4.7-8.1c-6.1-11-11.4-22.4-15.8-34.3c-3.2-8.7-.5-18.4 6.4-24.6l43.3-39.4C64.6 273.1 64 264.6 64 256s.6-17.1 1.7-25.4L22.4 191.2c-6.9-6.2-9.6-15.9-6.4-24.6c4.4-11.9 9.7-23.3 15.8-34.3l4.7-8.1c6.6-11 14-21.4 22.1-31.2c5.9-7.2 15.7-9.6 24.5-6.8l55.7 17.7c13.4-10.3 28.2-18.9 44-25.4l12.5-57.1c2-9.1 9-16.3 18.2-17.8C227.3 1.2 241.5 0 256 0s28.7 1.2 42.5 3.5c9.2 1.5 16.2 8.7 18.2 17.8l12.5 57.1c15.8 6.5 30.6 15.1 44 25.4l55.7-17.7c8.8-2.8 18.6-.3 24.5 6.8c8.1 9.8 15.5 20.2 22.1 31.2l4.7 8.1c6.1 11 11.4 22.4 15.8 34.3zM256 336a80 80 0 1 0 0-160 80 80 0 1 0 0 160z" />
                     </svg>
                     <MenuCard isVisible={menuCardIsVisible}>
-                      <li>Modify tool</li>
+                      <li onClick={setUpdateToolModalIsShown}>Modify tool</li>
                       <li onClick={setDeleteToolModalIsShown}>Delete tool</li>
                     </MenuCard>
                   </div>
@@ -134,12 +149,24 @@ export default function ToolCard({
           </div>
           <div className={styles.horizontalBar}></div>
           <div className={styles.tagsContainer}>
-            {tags.map((tag, id) => (
+            {toolTags.map((tag, id) => (
               <Tag key={id}>{tag.name}</Tag>
             ))}
           </div>
         </div>
       </a>
+      <UpdateToolForm
+        formIsOpen={updateToolModalIsShown}
+        hideModal={hideUpdateToolModal}
+        requestMethod="PATCH"
+        requestUrlPath={`/tools/${id}`}
+        selectedTagsAddToolForm={selectedTagsAddToolForm}
+        allTags={allTags}
+        id={id}
+        setToolsFrontend={setToolsFrontend}
+        handleAddTag={handleAddTag}
+        handleRemoveTag={handleRemoveTag}
+      />
       <DeleteToolForm
         description={description}
         formIsOpen={deleteToolModalIsShown}
