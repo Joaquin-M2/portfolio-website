8e6de4d958d2ac5ec3d284201ec2f9a4bd2c001d
Joaquin-M2 <joaquin.mmol@gmail.com>
2023-11-11 19:46:00 +0100

    Create "UpdateTagForm" component

---
+++ src/app/tools/UpdateTagForm.tsx
@@ -0,0 +1,157 @@
+"use client";
+
+import React, {
+  Dispatch,
+  MouseEventHandler,
+  SetStateAction,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
+import Modal from "../../components/Modal/Modal";
+import Form from "../../components/Form/Form";
+import Input from "../../components/Form/Input/Input";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { createRequest } from "../../utils/requests";
+
+interface UpdateToolFormProps {
+  formIsOpen: boolean;
+  hideModal: MouseEventHandler;
+  id?: string;
+  resetFormValues?: boolean;
+  setToolsFrontend?: Dispatch<SetStateAction<any[]>>;
+  tags?: any[];
+}
+
+interface FormInputs {
+  name: string;
+}
+
+function UpdateTagForm({
+  formIsOpen,
+  hideModal,
+  id,
+  resetFormValues,
+  setToolsFrontend,
+  tags,
+}: UpdateToolFormProps) {
+  const [formResponse, setFormResponse] = useState({
+    status: 500,
+    message: "",
+  });
+  const [requestIsSuccessful, setRequestIsSuccessful] = useState(false);
+  const selectSingleInput = useRef<HTMLSelectElement>();
+
+  useEffect(() => {
+    if (formIsOpen) {
+      setValue("name", selectSingleInput.current.selectedOptions[0].innerText);
+    }
+    if (!formIsOpen) {
+      reset();
+      setTimeout(() => {
+        setFormResponse((prevValue) => ({ ...prevValue, message: "" }));
+        setRequestIsSuccessful(false);
+      }, 500); // Time until CSS transition finishes.
+    }
+  }, [formIsOpen]);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+    setValue,
+    watch,
+  } = useForm<FormInputs>();
+
+  const onSubmit: SubmitHandler<FormInputs> = async (data) => {
+    try {
+      const response = await fetch(
+        createRequest({
+          urlPath: `/tags/${selectSingleInput.current.value}`,
+          method: "PATCH",
+          requestBody: data,
+        })
+      );
+
+      const result = await response.json();
+
+      setFormResponse({
+        status: response.status,
+        message: result.message,
+      });
+
+      if (response.status >= 200 && response.status < 400) {
+        setRequestIsSuccessful(true);
+      }
+      setToolsFrontend((prevValue) => [...prevValue, id]);
+    } catch (error) {
+      setFormResponse({
+        status: 500,
+        message: error.message,
+      });
+      throw error.message;
+    }
+  };
+
+  return (
+    <Modal
+      backendResponse={formResponse}
+      isShown={formIsOpen}
+      hideModal={hideModal}
+      targetForm={`update-tag-form-${id}`}
+      title="Update Tag"
+      requestIsSuccessful={requestIsSuccessful}
+    >
+      <Form
+        hasFieldset
+        id={`update-tag-form-${id}`}
+        legend="Update tag form"
+        onSubmit={handleSubmit(onSubmit)}
+        resetFormValues={resetFormValues}
+      >
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+            gap: "2rem",
+            margin: "0 1rem",
+          }}
+        >
+          <Input
+            allTags={tags}
+            formIsOpen={formIsOpen}
+            placeholder="Choose tag"
+            type="selectSingle"
+            ref={selectSingleInput}
+            onChange={() =>
+              setValue(
+                "name",
+                selectSingleInput.current.selectedOptions[0].innerText
+              )
+            }
+          />
+          <Input
+            aria-invalid={errors.name ? true : false}
+            watchedValue={watch("name")}
+            error={
+              errors.name && "Tag name needs to be between 2 and 50 characters."
+            }
+            formIsOpen={formIsOpen}
+            id="update-tag-title-input"
+            placeholder="Update tag name"
+            required
+            type="text"
+            {...register("name", {
+              required: true,
+              minLength: 2,
+              maxLength: 50,
+            })}
+          />
+        </div>
+      </Form>
+    </Modal>
+  );
+}
+
+export default UpdateTagForm;
