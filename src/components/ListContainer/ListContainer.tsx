e6fda99acc310bf2e7ebcf16bd1a9d27413e8c35
Joaquin-M2 <joaquin.mmol@gmail.com>
2023-11-20 06:39:56 +0100

    Create "ListContainer" component

---
+++ src/components/ListContainer/ListContainer.tsx
@@ -0,0 +1,59 @@
+import { useState } from "react";
+import Image from "next/image";
+
+import styles from "./listContainer.module.scss";
+
+interface ListContainerProps {
+  items: any[];
+  title: string;
+  usesIconPreview?: boolean;
+}
+
+function ListContainer({ items, title, usesIconPreview }: ListContainerProps) {
+  const [iconIsShown, setIconIsShown] = useState(
+    items.map(() => {
+      return false;
+    })
+  );
+
+  function handleShowIcon(id) {
+    let iconsState = [...iconIsShown];
+    iconsState[id] = !iconsState[id];
+    setIconIsShown(iconsState);
+  }
+
+  return (
+    <div className={styles.mainContainer}>
+      <h5 className={styles.title}>{title}</h5>
+      <div className={styles.innerContainer}>
+        <ul className={styles.list}>
+          {items.map((item, id) => {
+            return (
+              <div className={styles.itemContainer} key={item._id}>
+                <li
+                  className={`${styles.item} ${
+                    usesIconPreview && styles.itemIsClickable
+                  }`}
+                  onClick={() => usesIconPreview && handleShowIcon(id)}
+                >
+                  {item.name}
+                </li>
+                {usesIconPreview && iconIsShown[id] && (
+                  <Image
+                    src={item.url}
+                    alt={`Icon for ${item.name}`}
+                    width={50}
+                    height={50}
+                    className={styles.icon}
+                  />
+                )}
+              </div>
+            );
+          })}
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default ListContainer;
