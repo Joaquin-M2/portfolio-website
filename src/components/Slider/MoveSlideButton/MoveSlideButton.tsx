50b80f886e65bd7f782e13686bf846f93d4d6333
Joaquin-M2 <joaquin.mmol@gmail.com>
2023-12-11 06:06:21 +0100

    Refactor "MoveSlideButton" component
    So the direction of the arrow depends on a prop and not on CSS styling wherever this component may be used.

--- src/components/Slider/MoveSlideButton/MoveSlideButton.tsx
+++ src/components/Slider/MoveSlideButton/MoveSlideButton.tsx
@@ -1,12 +1,28 @@
-import styles from './moveSlideButton.module.scss';
+import styles from "./moveSlideButton.module.scss";
 
-export default function MoveSlideButton(props) {
+interface MoveSlideButtonProps {
+  additionalStyles?: string;
+  changeSlide?: () => void;
+  leftwardsArrow?: boolean;
+}
+
+export default function MoveSlideButton({
+  additionalStyles,
+  changeSlide,
+  leftwardsArrow,
+}: MoveSlideButtonProps) {
   return (
     <button
-      className={`${styles.Button} ${props.additionalStyles}`}
-      onClick={props.changeSlide}
+      className={`${styles.Button} ${additionalStyles}`}
+      onClick={changeSlide}
     >
-      <div className={styles.ButtonContentContainer}>&#10148;</div>
+      <div
+        className={
+          leftwardsArrow ? styles.LeftwardsArrow : styles.RightwardsArrow
+        }
+      >
+        &#10148;
+      </div>
     </button>
   );
 }
