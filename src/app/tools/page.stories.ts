fc857487ea9217c71785bb3f4ce51b34ddd74d69
Joaquin-M2 <joaquin.mmol@gmail.com>
2023-12-17 23:35:32 +0100

    Create story for "Tools" page

---
+++ src/app/tools/page.stories.ts
@@ -0,0 +1,13 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import Page from "./page";
+
+const meta = {
+  title: "Pages/Tools page",
+  component: Page,
+} satisfies Meta<typeof Page>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Main: Story = {};
