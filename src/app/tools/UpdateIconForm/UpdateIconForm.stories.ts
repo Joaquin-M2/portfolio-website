13327daa49bf3f16aa59830b10505ef8396f7b31
Joaquin-M2 <joaquin.mmol@gmail.com>
2023-12-19 20:56:15 +0100

    Create Base story for 'UpdateIconForm' component

---
+++ src/app/tools/UpdateIconForm/UpdateIconForm.stories.ts
@@ -0,0 +1,62 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { faker } from "@faker-js/faker";
+
+import UpdateIconForm from "./UpdateIconForm";
+
+const meta = {
+  title: "Pages/Tools page/Update Icon Form",
+  component: UpdateIconForm,
+  parameters: {
+    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
+    layout: "fullscreen",
+  },
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
+  tags: ["autodocs"],
+  // More on argTypes: https://storybook.js.org/docs/api/argtypes
+  argTypes: {
+    formIsOpen: {
+      description: "Checks if the modal form is open.",
+    },
+    hideModal: {
+      description: "Function that closes the modal.",
+    },
+    resetFormValues: {
+      description: "Sets if the inputs in the form should be reset.",
+    },
+    setToolsFrontend: {
+      description:
+        "Forces a rerender of the tools shown on the frontend when a tool is deleted.",
+    },
+    icons: {
+      description:
+        "Elements (icons + their names) that will be displayed on the list for the user to select.",
+    },
+  },
+} satisfies Meta<typeof UpdateIconForm>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+const randomNamesSet = new Set(
+  Array(20)
+    .fill(null)
+    .map(() => faker.vehicle.manufacturer())
+);
+
+const randomIcons = [...randomNamesSet]
+  .sort((a, b) => a.localeCompare(b))
+  .map((iconName) => ({
+    name: iconName,
+    url: faker.image.urlLoremFlickr({ width: 60, height: 60 }),
+    _id: faker.string.uuid(),
+  }));
+
+export const Base: Story = {
+  args: {
+    formIsOpen: true,
+    //hideModal: () => {},
+    resetFormValues: false,
+    //setToolsFrontend: () => {},
+    icons: randomIcons,
+  },
+};
