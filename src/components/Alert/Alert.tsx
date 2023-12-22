fa7b023dd90cd6797c41c86ed79723fdff3ada4b
Joaquin-M2 <joaquin.mmol@gmail.com>
2023-12-22 19:18:26 +0100

    Create 'Alert' component

---
+++ src/components/Alert/Alert.tsx
@@ -0,0 +1,28 @@
+import styles from "./alert.module.scss";
+
+interface AlertProps {
+  backendResponse: { message: string; status: number };
+}
+
+function Alert({ backendResponse }: AlertProps) {
+  return (
+    <div className={styles.wrapper}>
+      <p
+        className={`${styles.baseStyling} ${
+          backendResponse.message &&
+          backendResponse.status >= 400 &&
+          styles.error
+        } ${
+          backendResponse.message &&
+          backendResponse.status < 400 &&
+          styles.success
+        }`}
+        role="alert"
+      >
+        {backendResponse.message}
+      </p>
+    </div>
+  );
+}
+
+export default Alert;
