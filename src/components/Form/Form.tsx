74049e7d9c17789630615344f0d60cb5bc852e78
Joaquin-M2 <joaquin.mmol@gmail.com>
2023-12-22 19:21:52 +0100

    Add 'Alert' component, 'backendResponse' and 'isPending' props to 'Form' component

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -4,6 +4,7 @@ import React, { FormEventHandler, useEffect, useRef } from "react";
 
 import styles from "./form.module.scss";
 import Button from "../Button/Button";
+import Alert from "../Alert/Alert";
 
 interface FormProps {
   children: JSX.Element | JSX.Element[];
@@ -14,6 +15,8 @@ interface FormProps {
   resetFormValues?: boolean;
   usesItsOwnButtons?: boolean;
   onReset?: () => void;
+  backendResponse?: { message: string; status: number };
+  isPending?: boolean;
 }
 
 function Form({
@@ -25,6 +28,8 @@ function Form({
   resetFormValues,
   usesItsOwnButtons,
   onReset,
+  backendResponse,
+  isPending,
 }: FormProps) {
   const form = useRef<HTMLFormElement>();
 
@@ -33,13 +38,20 @@ function Form({
   }, [resetFormValues]);
 
   const formButtons = (
-    <div className={styles.buttonsContainer}>
-      <Button form={id} type="submit">
-        Send
-      </Button>
-      <Button form={id} type="reset" onClick={onReset}>
-        Clear
-      </Button>
+    <div className={styles.bottomSectionContainer}>
+      {isPending ? (
+        <p className={styles.loader}>Sending...</p>
+      ) : (
+        <Alert backendResponse={backendResponse} />
+      )}
+      <div className={styles.buttonsContainer}>
+        <Button form={id} type="submit" disabled={isPending}>
+          Send
+        </Button>
+        <Button form={id} type="reset" disabled={isPending} onClick={onReset}>
+          Clear
+        </Button>
+      </div>
     </div>
   );
 
