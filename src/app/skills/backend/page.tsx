ac57f1975453b0ed9cdb3970ccefa8baa80d90fc
Joaquin-M2 <joaquin.mmol@gmail.com>
2024-01-03 14:46:12 +0100

    Use new "SkillBar" component in "backend skills" page + Update skills

--- src/app/skills/backend/page.tsx
+++ src/app/skills/backend/page.tsx
@@ -1,154 +1,71 @@
 "use client";
 
-import { useMemo, useState } from "react";
+import { useMemo } from "react";
 
 import SkillsBlock from "@/components/SkillsBlock/SkillsBlock";
 import SkillBar from "@/components/SkillsBlock/SkillBar/SkillBar";
-import SkillModal from "@/components/SkillsBlock/SkillModal/SkillModal";
 
-import tech from "@/data/techs-names";
-import technologies from "@/data/skills-backend";
-
-import stylesBackendPage from "./backendPage.module.scss";
-import styles from "../skills.module.scss";
+import styles from "./backendPage.module.scss";
 
 function Page() {
-  const [technology, setTechnology] = useState(tech.SQL);
-  const [modalIsShown, setModalIsShown] = useState(false);
-  function showModal(event) {
-    setModalIsShown(true);
-
-    setTechnology(event.target.closest("li").querySelector("span").textContent);
-  }
-
-  const currentTechnology = technologies.find(
-    (tech) => tech.name === technology
-  );
-
-  ////////////////////////////////////////////////////////////////////
-  // FUNCTION TRYING TO SIMPLIFY (DRY) THE CREATION OF SKILL BLOCKS
-  // Unfinished
-
-  // function block(...[liContent, progressPercentage, nesting]) {
-  //   let mainUl = <ul className={`${styles.unorderedList} ${styles.mainUnorderedList}`}></ul>
-  //   let nestingLi = <li className={styles.listElement}>
-  //                     <div className={styles.listElementContainer} onClick={(event) => showModal(event)}>
-  //                       <SkillBar progressPercentage={progressPercentage}>
-  //                         {liContent}
-  //                       </SkillBar>
-  //                     </div>
-  //                     {li}
-  //                   </li>
-  //   let ul = <ul className={styles.unorderedList}></ul>
-  //   let li = <li className={styles.listElement}>
-  //               <div className={styles.listElementContainer} onClick={(event) => showModal(event)}>
-  //                 <SkillBar progressPercentage={progressPercentage}>
-  //                   {liContent}
-  //                 </SkillBar>
-  //               </div>
-  //             </li>
-  //   const skillsetPerDepthLevel = []
-  //   for (const skill of [liContent, progressPercentage, nesting]) {
-  //     if (!nesting) {
-  //       skillsetPerDepthLevel.push(li);
-  //     } else {
-
-  //     }
-  //   }
-  // }
-
   // It is necessary to use the "useMemo()" hook in order to avoid re-renders on the progress bars
   // whenever the "useState()" hook is updated.
   const skills = useMemo(() => {
     return (
       <>
-        <SkillsBlock additionalStyles={styles.SQLSkillsBlock}>
-          <ul className={`${styles.unorderedList} ${styles.mainUnorderedList}`}>
-            <li className={`${styles.listElement} ${styles.firstListElement}`}>
-              <div
-                className={`${styles.listElementContainer} ${styles.firstListElementContainer}`}
-                onClick={(event) => showModal(event)}
-              >
-                <SkillBar progressPercentage={65}>{tech.SQL}</SkillBar>
-              </div>
-              <ul className={styles.unorderedList}>
-                <li className={styles.listElement}>
-                  <div
-                    className={styles.listElementContainer}
-                    onClick={(event) => showModal(event)}
-                  >
-                    <SkillBar progressPercentage={80}>{tech.NODEJS}</SkillBar>
-                  </div>
-                  <ul className={styles.unorderedList}>
-                    <li className={styles.listElement}>
-                      <div
-                        className={styles.listElementContainer}
-                        onClick={(event) => showModal(event)}
-                      >
-                        <SkillBar progressPercentage={70}>
-                          {tech.EXPRESS}
-                        </SkillBar>
-                      </div>
-                    </li>
-                    <li className={styles.listElement}>
-                      <div
-                        className={styles.listElementContainer}
-                        onClick={(event) => showModal(event)}
-                      >
-                        <SkillBar progressPercentage={65}>
-                          {tech.SEQUELIZE}
-                        </SkillBar>
-                      </div>
-                    </li>
-                  </ul>
-                </li>
-              </ul>
-            </li>
-          </ul>
+        <SkillsBlock additionalStyles={styles.nodeSkillsBlock}>
+          <SkillBar
+            color="blue"
+            title="Node.js"
+            progressPercentage={65}
+            href="https://nodejs.org/en"
+          >
+            <SkillBar
+              color="blue"
+              title="Express"
+              progressPercentage={60}
+              isSubLevel
+              href="https://expressjs.com/"
+            />
+            <SkillBar
+              color="blue"
+              title="Mongoose"
+              progressPercentage={60}
+              isSubLevel
+              href="https://mongoosejs.com/"
+            />
+            <SkillBar
+              color="blue"
+              title="Sequelize"
+              progressPercentage={50}
+              isSubLevel
+              href="https://sequelize.org/"
+            />
+          </SkillBar>
         </SkillsBlock>
-        <SkillsBlock additionalStyles={styles.NoSQLSkillsBlock}>
-          <ul className={`${styles.unorderedList} ${styles.mainUnorderedList}`}>
-            <li className={`${styles.listElement} ${styles.firstListElement}`}>
-              <div
-                className={`${styles.listElementContainer} ${styles.firstListElementContainer}`}
-                onClick={(event) => showModal(event)}
-              >
-                <SkillBar progressPercentage={65}>{tech.NOSQL}</SkillBar>
-              </div>
-              <ul className={styles.unorderedList}>
-                <li className={styles.listElement}>
-                  <div
-                    className={styles.listElementContainer}
-                    onClick={(event) => showModal(event)}
-                  >
-                    <SkillBar progressPercentage={80}>{tech.NODEJS}</SkillBar>
-                  </div>
-                  <ul className={styles.unorderedList}>
-                    <li className={styles.listElement}>
-                      <div
-                        className={styles.listElementContainer}
-                        onClick={(event) => showModal(event)}
-                      >
-                        <SkillBar progressPercentage={70}>
-                          {tech.EXPRESS}
-                        </SkillBar>
-                      </div>
-                    </li>
-                    <li className={styles.listElement}>
-                      <div
-                        className={styles.listElementContainer}
-                        onClick={(event) => showModal(event)}
-                      >
-                        <SkillBar progressPercentage={65}>
-                          {tech.MONGOOSE}
-                        </SkillBar>
-                      </div>
-                    </li>
-                  </ul>
-                </li>
-              </ul>
-            </li>
-          </ul>
+        <SkillsBlock additionalStyles={styles.pythonSkillsBlock}>
+          <SkillBar
+            color="blue"
+            title="Python"
+            progressPercentage={55}
+            href="https://www.python.org/"
+          >
+            <SkillBar
+              color="blue"
+              title="Django"
+              progressPercentage={50}
+              isSubLevel
+              href="https://www.djangoproject.com/"
+            >
+              <SkillBar
+                color="blue"
+                title="Django REST"
+                progressPercentage={50}
+                isSubLevel
+                href="https://www.django-rest-framework.org/"
+              />
+            </SkillBar>
+          </SkillBar>
         </SkillsBlock>
       </>
     );
@@ -156,18 +73,8 @@ function Page() {
 
   return (
     <>
-      <div className={stylesBackendPage.positionMainElements}>
-        <main className={stylesBackendPage.MainContainer}>
-          {skills}
-          <SkillModal
-            modalIsShown={modalIsShown}
-            closeFromBackdrop={() => setModalIsShown(false)}
-            closeFromButton={() => setModalIsShown(false)}
-            title={currentTechnology.name}
-          >
-            {currentTechnology.description}
-          </SkillModal>
-        </main>
+      <div className={styles.positionMainElements}>
+        <main className={styles.MainContainer}>{skills}</main>
       </div>
     </>
   );
