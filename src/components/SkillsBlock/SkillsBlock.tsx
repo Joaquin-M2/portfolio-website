05da0b8b083e1da710ede80f3bd94069067d20de
Joaquin-M2 <joaquin.mmol@gmail.com>
2024-01-04 12:36:21 +0100

    Add "color" prop to "SkillsBlock" component

--- src/components/SkillsBlock/SkillsBlock.tsx
+++ src/components/SkillsBlock/SkillsBlock.tsx
@@ -3,13 +3,21 @@ import styles from "./skillsBlock.module.scss";
 interface SkillsBlockProps {
   children: JSX.Element | JSX.Element[];
   additionalStyles?: string;
+  color: "red" | "green" | "yellow";
 }
 
 export default function SkillsBlock({
   additionalStyles,
+  color = "red",
   children,
 }: SkillsBlockProps) {
   return (
-    <div className={`${styles.Container} ${additionalStyles}`}>{children}</div>
+    <div
+      className={`${styles.Container} ${color === "red" && styles.red} ${
+        color === "green" && styles.green
+      } ${color === "yellow" && styles.yellow} ${additionalStyles}`}
+    >
+      {children}
+    </div>
   );
 }
