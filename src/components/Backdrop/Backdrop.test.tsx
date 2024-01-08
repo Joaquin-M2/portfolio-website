98476422951b5abcdb1b3359d2ff21787fec3215
Joaquin-M2 <joaquin.mmol@gmail.com>
2024-01-08 19:25:41 +0100

    Create unit tests for the "Backdrop" component

---
+++ src/components/Backdrop/Backdrop.test.tsx
@@ -0,0 +1,29 @@
+import "@testing-library/jest-dom";
+import { render, screen } from "@testing-library/react";
+import Backdrop from "./Backdrop";
+import userEvent from "@testing-library/user-event";
+
+const mockedHideBackdropFunction = jest.fn();
+
+describe("<Backdrop /> component", () => {
+  it("is rendered", () => {
+    render(
+      <Backdrop isShown={true} hideBackdrop={mockedHideBackdropFunction} />
+    );
+
+    const backdrop = screen.getByRole("presentation");
+
+    expect(backdrop).toBeInTheDocument();
+  });
+
+  it("calls the 'hideBackdrop' function when it is clicked", async () => {
+    render(
+      <Backdrop isShown={true} hideBackdrop={mockedHideBackdropFunction} />
+    );
+
+    const backdrop = screen.getByRole("presentation");
+    await userEvent.click(backdrop);
+
+    expect(mockedHideBackdropFunction).toHaveBeenCalled();
+  });
+});
