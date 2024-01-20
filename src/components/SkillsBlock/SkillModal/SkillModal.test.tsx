1ae17d9de3d7e745808702b41fbc7cc07fd4fd95
Joaquin-M2 <joaquin.mmol@gmail.com>
2024-01-20 06:32:43 +0100

    Create unit tests for 'SkillModal' component

---
+++ src/components/SkillsBlock/SkillModal/SkillModal.test.tsx
@@ -0,0 +1,61 @@
+import { render, screen } from "@testing-library/react";
+import SkillModal from "./SkillModal";
+import { userEvent } from "@storybook/test";
+
+const closeFromButtonFunction = jest.fn();
+const closeFromBackdropFunction = jest.fn();
+
+describe("<SkillModal /> component", () => {
+  it("can be rendered", () => {
+    render(
+      <SkillModal
+        title="Dummy title"
+        modalIsShown={true}
+        closeFromButton={closeFromButtonFunction}
+        closeFromBackdrop={closeFromBackdropFunction}
+      >
+        <p>SkillModal content</p>
+      </SkillModal>
+    );
+
+    const skillModal = screen.getByRole("dialog");
+
+    expect(skillModal).toBeInTheDocument();
+  });
+
+  it("can trigger the 'closeFromButton' function", async () => {
+    render(
+      <SkillModal
+        title="Dummy title"
+        modalIsShown={true}
+        closeFromButton={closeFromButtonFunction}
+        closeFromBackdrop={closeFromBackdropFunction}
+      >
+        <p>SkillModal content</p>
+      </SkillModal>
+    );
+
+    const closeButton = screen.getByRole("button");
+    await userEvent.click(closeButton);
+
+    expect(closeFromButtonFunction).toHaveBeenCalled();
+  });
+
+  it("can trigger the 'closeFromBackdrop' function", async () => {
+    render(
+      <SkillModal
+        title="Dummy title"
+        modalIsShown={true}
+        closeFromButton={closeFromButtonFunction}
+        closeFromBackdrop={closeFromBackdropFunction}
+      >
+        <p>SkillModal content</p>
+      </SkillModal>
+    );
+
+    const backdrop = screen.getByRole("presentation");
+    await userEvent.click(backdrop);
+
+    expect(closeFromBackdropFunction).toHaveBeenCalled();
+  });
+});
