425678ec5b83c257829c1e17b6c1ad2ecfb12482
Joaquin-M2 <joaquin.mmol@gmail.com>
2024-01-20 06:36:59 +0100

    Fix "FilterButton" 'Base' story

--- src/components/FiltersBar/FilterButton/FilterButton.stories.ts
+++ src/components/FiltersBar/FilterButton/FilterButton.stories.ts
@@ -16,4 +16,8 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 // More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
-export const Base: Story = {};
+export const Base: Story = {
+  args: {
+    children: "Test",
+  },
+};
