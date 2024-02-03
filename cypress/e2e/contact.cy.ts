acf978f9f0a0a34023cf15092a00ac3f89a1c91a
Joaquin-M2 <joaquin.mmol@gmail.com>
2024-02-03 06:07:40 +0100

    Create first E2E test for the "Contact" page

---
+++ cypress/e2e/contact.cy.ts
@@ -0,0 +1,21 @@
+describe("Contact page", () => {
+  beforeEach(() => {
+    cy.visit("/contact");
+  });
+
+  it("can send a contact form", () => {
+    cy.intercept("POST", "https://api.web3forms.com/submit", {
+      status: 201,
+      success: true,
+    }).as("submit");
+    cy.getByData("contact-name-input").type("Joaquín");
+    cy.getByData("contact-email-input").type("test-email@test.com");
+    cy.getByData("contact-subject-input").type("E2E contact form testing");
+    cy.getByData("contact-message-input").type(
+      "This is a test made with Cypress."
+    );
+    cy.get('[type="submit"]').click({ force: true });
+    cy.wait("@submit");
+    cy.getByData("alert-message").should("exist");
+  });
+});
