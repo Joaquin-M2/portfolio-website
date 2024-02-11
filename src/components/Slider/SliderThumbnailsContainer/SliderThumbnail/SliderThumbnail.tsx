9409d83734b70429ee85ec35eb05cd8a73395d38
Joaquin-M2 <joaquin.mmol@gmail.com>
2024-02-11 14:30:49 +0100

    Refactor "SliderThumbnail" component
    It was unnecessary complex and it was throwing some errors related to user events (onChange instead of onClick).

--- src/components/Slider/SliderThumbnailsContainer/SliderThumbnail/SliderThumbnail.tsx
+++ src/components/Slider/SliderThumbnailsContainer/SliderThumbnail/SliderThumbnail.tsx
@@ -3,49 +3,35 @@ import Image, { StaticImageData } from "next/image";
 import styles from "./sliderThumbnail.module.scss";
 
 interface SliderThumbnailProps {
-  forAttribute: string;
-  id: string;
   image: string | StaticImageData;
   updateStateForActiveThumbnail: () => void;
   setButtonIsChecked: boolean;
 }
 
 export default function SliderThumbnail({
-  forAttribute,
-  id,
   image,
   updateStateForActiveThumbnail,
   setButtonIsChecked,
 }: SliderThumbnailProps) {
   return (
     <>
-      <input
-        className={styles.InputThumbnailImageContainer}
-        id={forAttribute}
-        onChange={updateStateForActiveThumbnail}
-        checked={setButtonIsChecked}
-        type="radio"
-        name="slider thumbnail"
-      />
-      <label
+      <div
         data-testid="sliderThumbnailLabel"
-        className={`${styles.LabelThumbnailImageContainer} ${
+        className={`${styles.wrapper} ${
           setButtonIsChecked && styles.activeThumbnail
         }`}
-        htmlFor={forAttribute}
+        onClick={updateStateForActiveThumbnail}
       >
-        <div className={styles.ThumbnailImageContainer}>
-          <Image
-            className={`${styles.ThumbnailImageContainer_Image} ${
-              !setButtonIsChecked && styles.inactiveThumbnail
-            }`}
-            src={`${image}-thumbnail.jpg` || image}
-            alt={`Thumbnail image for ${image}`}
-            width={60}
-            height={60}
-          />
-        </div>
-      </label>
+        <Image
+          className={`${styles.image} ${
+            !setButtonIsChecked && styles.inactiveThumbnail
+          }`}
+          src={`${image}-thumbnail.jpg` || image}
+          alt={`Thumbnail image for ${image}`}
+          width={60}
+          height={60}
+        />
+      </div>
     </>
   );
 }
