03d069287b8888b83e8fb6886875a70c42224633
Joaquin-M2 <joaquin.mmol@gmail.com>
2024-02-15 19:43:36 +0100

    Use ".beforeEach()" on Home page E2E test

--- e2e/home.spec.ts
+++ e2e/home.spec.ts
@@ -1,13 +1,19 @@
 import { test, expect } from "@playwright/test";
 
-test('"my portfolio" button takes to the right page', async ({ page }) => {
+test.beforeEach(async ({ page }) => {
   await page.goto("/");
+});
+
+test('will take to the "Portfolio" page after clicking on the "my portfolio" button', async ({
+  page,
+}) => {
   await page.getByRole("button", { name: "my portfolio" }).click();
   await expect(page).toHaveURL(/.*\/portfolio\/projects/);
 });
 
-test('"drop me a line" button takes to the right page', async ({ page }) => {
-  await page.goto("/");
+test('will take to the "Contact" page after clicking on the "drop me a line" button', async ({
+  page,
+}) => {
   await page.getByRole("button", { name: "drop me a line" }).click();
   await expect(page).toHaveURL(/.*\/contact/);
 });
