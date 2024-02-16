600486ca826f62330a8acbf2aea7e813eb0f1fa4
Joaquin-M2 <joaquin.mmol@gmail.com>
2024-02-16 20:06:30 +0100

    Add prefix "/api/v1" to requests paths

--- src/utils/api-backend-url-domain.ts
+++ src/utils/api-backend-url-domain.ts
@@ -1,9 +1,9 @@
 export default function getApiBackendUrlDomain() {
   if (typeof window !== "undefined") {
     if (window.location.hostname === "localhost") {
-      return "http://localhost:4000";
+      return "http://localhost:4000/api/v1";
     } else {
-      return "https://portfolio-website-backend.adaptable.app";
+      return "https://portfolio-website-backend.adaptable.app/api/v1";
     }
   }
 }
