581359c00876801461a70c317ed63efe779327f4
Joaquin-M2 <joaquin.mmol@gmail.com>
2024-02-18 21:08:14 +0100

    Add new E2E tests for "Tools" page

--- e2e/tools.spec.ts
+++ e2e/tools.spec.ts
@@ -33,7 +33,29 @@ test.beforeEach(async ({ page }) => {
   await page.goto("/tools");
 });
 
+test.describe("Common to ANY user (i.e. not logged in, logged in and admin)", () => {
+  test("can filter tools by text and tag and open one of the results", async ({
+    page,
+  }) => {
+    await page.getByRole("checkbox", { name: "FILTERS" }).click();
+    await page.getByPlaceholder("Search by tool title - Case").click();
+    await page.getByPlaceholder("Search by tool title - Case").fill("tool");
+    await page
+      .getByRole("complementary")
+      .getByRole("option", { name: "Accessibility" })
+      .click();
+    await page.getByRole("checkbox", { name: "FILTERS" }).click();
+
+    await expect(page.getByRole("main").getByRole("link")).toHaveCount(1);
+  });
+});
+
 test.describe("User is NOT logged in", () => {
+  test("'Log In' and 'Sign Up' buttons are visible", async ({ page }) => {
+    await expect(page.getByRole("button", { name: "Log In" })).toBeVisible();
+    await expect(page.getByRole("button", { name: "Sign Up" })).toBeVisible();
+  });
+
   test("can save tools and keep them as favorite after reloading the page", async ({
     page,
   }) => {
@@ -64,5 +86,57 @@ test.describe("User is NOT logged in", () => {
     await expect(
       page.getByRole("link").nth(1).getByRole("heading")
     ).toContainText("Title #1234");
+  });
+
+  test("can let users to sign up", async ({ page }) => {
+    // Mock the response (SIGN UP).
+    await page.route("*/**/api/v1/user/signup", async (route) => {
+      const json = {
+        status: 201,
+        message: "✅ User created successfully ✅",
+      };
+      await route.fulfill({ json });
+    });
+    await page.getByRole("button", { name: "Sign Up" }).click();
+    await page.getByLabel("Sign Up").getByPlaceholder("Email").click();
+    await page
+      .getByLabel("Sign Up")
+      .getByPlaceholder("Email")
+      .fill("test@test.com");
+    await page.getByLabel("Sign Up").getByPlaceholder("Email").press("Tab");
+    await page
+      .getByLabel("Sign Up")
+      .getByPlaceholder("Password")
+      .fill("test1234");
+    await page.getByRole("button", { name: "Accept" }).click();
+
+    await expect(page.getByText("User created successfully")).toBeInViewport();
+  });
+
+  test("can let users to log in", async ({ page }) => {
+    // Mock the response (LOG IN).
+    await page.route("*/**/api/v1/user/login", async (route) => {
+      const json = {
+        status: 201,
+        message: "✅ Logged in ✅",
+        token: "1234",
+        favoriteTools: [],
+      };
+      await route.fulfill({ json });
+    });
+    await page.getByRole("button", { name: "Log In" }).click();
+    await page.getByLabel("Log In").getByPlaceholder("Email").click();
+    await page
+      .getByLabel("Log In")
+      .getByPlaceholder("Email")
+      .fill("test@test.com");
+    await page.getByLabel("Log In").getByPlaceholder("Email").press("Tab");
+    await page
+      .getByLabel("Log In")
+      .getByPlaceholder("Password")
+      .fill("test1234");
+    await page.getByRole("button", { name: "Accept" }).click();
+
+    await expect(page.getByText("Logged in")).toBeInViewport();
   });
 });
