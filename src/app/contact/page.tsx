9baf6fb5ae35ed6e7110e612fbc31c178dc242e9
Joaquin-M2 <joaquin.mmol@gmail.com>
2024-02-27 16:01:18 +0100

    Fix responsiveness on big screens ("Contact" page)

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -84,136 +84,144 @@ export default function Contact() {
       <div className={styles.positionMainElements}>
         <main className={styles.Container}>
           <div className={styles.FormContainer}>
-            <Form
-              hasFieldset
-              id="contact-form"
-              legend="Contact form"
-              onSubmit={handleSubmit(onSubmit)}
-              usesItsOwnButtons
-              backendResponse={formResponse}
-              isPending={formIsPending}
-              onReset={() => {
-                reset({
-                  name: "",
-                  email: "",
-                  subject: "",
-                  message: "",
-                });
-              }}
-            >
-              <input
-                type="hidden"
-                value="fe6697fb-809f-4b14-9754-3b0e1293af59"
-                {...register("access_key")}
-              />
-              <Input
-                data-cy="contact-name-input"
-                aria-invalid={errors.name ? true : false}
-                watchedValue={watch("name")}
-                error={
-                  errors.name && "Name needs to be between 3 and 50 characters."
-                }
-                formIsOpen
-                placeholder="Your name"
-                required
-                type="text"
-                {...register("name", {
-                  required: true,
-                  minLength: 3,
-                  maxLength: 50,
-                })}
-              />
-              <Input
-                data-cy="contact-email-input"
-                aria-invalid={errors.email ? true : false}
-                watchedValue={watch("email")}
-                error={
-                  errors.email &&
-                  "Invalid email. Accepted format: <username>@<domain>"
-                }
-                formIsOpen
-                placeholder="Your email"
-                required
-                type="email"
-                {...register("email", { required: true, pattern: emailRegExp })}
-              />
-              <Input
-                data-cy="contact-subject-input"
-                aria-invalid={errors.subject ? true : false}
-                watchedValue={watch("subject")}
-                error={
-                  errors.subject &&
-                  "Subject needs to be between 3 and 50 characters."
-                }
-                formIsOpen
-                placeholder="Message subject"
-                required
-                type="text"
-                {...register("subject", {
-                  required: true,
-                  minLength: 3,
-                  maxLength: 50,
-                })}
-              />
-              <Input
-                data-cy="contact-message-input"
-                aria-invalid={errors.message ? true : false}
-                watchedValue={watch("message")}
-                error={
-                  errors.message &&
-                  "Your message needs to be between 10 and 250 characters."
-                }
-                formIsOpen
-                placeholder="Message"
-                required
-                type="textarea"
-                {...register("message", {
-                  required: true,
-                  minLength: 10,
-                  maxLength: 250,
-                })}
-              />
-            </Form>
+            <div className={styles.formInnerContainer}>
+              <Form
+                hasFieldset
+                id="contact-form"
+                legend="Contact form"
+                onSubmit={handleSubmit(onSubmit)}
+                usesItsOwnButtons
+                backendResponse={formResponse}
+                isPending={formIsPending}
+                onReset={() => {
+                  reset({
+                    name: "",
+                    email: "",
+                    subject: "",
+                    message: "",
+                  });
+                }}
+              >
+                <input
+                  type="hidden"
+                  value="fe6697fb-809f-4b14-9754-3b0e1293af59"
+                  {...register("access_key")}
+                />
+                <Input
+                  data-cy="contact-name-input"
+                  aria-invalid={errors.name ? true : false}
+                  watchedValue={watch("name")}
+                  error={
+                    errors.name &&
+                    "Name needs to be between 3 and 50 characters."
+                  }
+                  formIsOpen
+                  placeholder="Your name"
+                  required
+                  type="text"
+                  {...register("name", {
+                    required: true,
+                    minLength: 3,
+                    maxLength: 50,
+                  })}
+                />
+                <Input
+                  data-cy="contact-email-input"
+                  aria-invalid={errors.email ? true : false}
+                  watchedValue={watch("email")}
+                  error={
+                    errors.email &&
+                    "Invalid email. Accepted format: <username>@<domain>"
+                  }
+                  formIsOpen
+                  placeholder="Your email"
+                  required
+                  type="email"
+                  {...register("email", {
+                    required: true,
+                    pattern: emailRegExp,
+                  })}
+                />
+                <Input
+                  data-cy="contact-subject-input"
+                  aria-invalid={errors.subject ? true : false}
+                  watchedValue={watch("subject")}
+                  error={
+                    errors.subject &&
+                    "Subject needs to be between 3 and 50 characters."
+                  }
+                  formIsOpen
+                  placeholder="Message subject"
+                  required
+                  type="text"
+                  {...register("subject", {
+                    required: true,
+                    minLength: 3,
+                    maxLength: 50,
+                  })}
+                />
+                <Input
+                  data-cy="contact-message-input"
+                  aria-invalid={errors.message ? true : false}
+                  watchedValue={watch("message")}
+                  error={
+                    errors.message &&
+                    "Your message needs to be between 10 and 250 characters."
+                  }
+                  formIsOpen
+                  placeholder="Message"
+                  required
+                  type="textarea"
+                  {...register("message", {
+                    required: true,
+                    minLength: 10,
+                    maxLength: 250,
+                  })}
+                />
+              </Form>
+            </div>
           </div>
           <div className={styles.OtherContactMeansContainer}>
-            <h2>Not a big fan of forms?</h2>
-            <h3>You can also contact me over here:</h3>
-            <ul>
-              <li>
-                <a
-                  href="mailto:joaquin.mmol@gmail.com"
-                  className={`${styles.EmailLogo} ${styles.ContactLogos}`}
-                  target="_blank"
-                >
-                  <EmailLogo />
-                  Email
-                </a>
-              </li>
-              <li>
-                <a
-                  href="https://www.linkedin.com/in/joaquin-m2/"
-                  className={`${styles.LinkedinLogo} ${styles.ContactLogos}`}
-                  target="_blank"
-                >
-                  <LinkedinLogo />
-                  LinkedIn
-                </a>
-              </li>
-              <li>
-                <a
-                  href="https://github.com/Joaquin-M2"
-                  className={`${styles.GithubLogo} ${styles.ContactLogos}`}
-                  target="_blank"
-                >
-                  <GithubLogo />
-                  GitHub
-                </a>
-              </li>
-            </ul>
-            <p className={styles.bottomParagraph}>
-              If you prefer a direct call, feel free to send me an email so we
-              can arrange a meeting.
-            </p>
+            <div className={styles.otherContactMeansInnerContainer}>
+              <h2>Not a big fan of forms?</h2>
+              <h3>You can also contact me over here:</h3>
+              <ul>
+                <li>
+                  <a
+                    href="mailto:joaquin.mmol@gmail.com"
+                    className={`${styles.EmailLogo} ${styles.ContactLogos}`}
+                    target="_blank"
+                  >
+                    <EmailLogo />
+                    Email
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="https://www.linkedin.com/in/joaquin-m2/"
+                    className={`${styles.LinkedinLogo} ${styles.ContactLogos}`}
+                    target="_blank"
+                  >
+                    <LinkedinLogo />
+                    LinkedIn
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="https://github.com/Joaquin-M2"
+                    className={`${styles.GithubLogo} ${styles.ContactLogos}`}
+                    target="_blank"
+                  >
+                    <GithubLogo />
+                    GitHub
+                  </a>
+                </li>
+              </ul>
+              <p className={styles.bottomParagraph}>
+                If you prefer a direct call, feel free to send me an email so we
+                can arrange a meeting.
+              </p>
+            </div>
           </div>
         </main>
       </div>
