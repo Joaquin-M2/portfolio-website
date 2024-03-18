bb4f504c22e63d705a749c0817a62968fb3af88c
Joaquin-M2 <joaquin.mmol@gmail.com>
2024-03-18 12:41:35 +0100

    Set Playwright configuration back to its default regarding browsers screen size
    Setting a max-height to modals fixed the error that made necessary to change the browser size on E2E tests.

--- playwright.config.ts
+++ playwright.config.ts
@@ -28,11 +28,6 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
-
-    // So the browser window is open full-sized.
-    launchOptions: {
-      args: ["--start-maximized"],
-    },
   },
 
   /* Configure projects for major browsers */
@@ -40,24 +35,21 @@ export default defineConfig({
     {
       name: "chromium",
       use: {
-        //...devices["Desktop Chrome"]
-        viewport: null, // So the browser window is open full-sized.
+        ...devices["Desktop Chrome"],
       },
     },
 
     {
       name: "firefox",
       use: {
-        //...devices["Desktop Firefox"]
-        viewport: null, // So the browser window is open full-sized.
+        ...devices["Desktop Firefox"],
       },
     },
 
     {
       name: "webkit",
       use: {
-        //...devices["Desktop Safari"]
-        viewport: null, // So the browser window is open full-sized.
+        ...devices["Desktop Safari"],
       },
     },
 
